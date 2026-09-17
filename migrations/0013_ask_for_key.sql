d7337cc441e004f514c782777312b222c78b2542
albinosloth21-source <albinosloth21@gmail.com>
2026-09-16 20:13:26 -0500

    Add iOS stubs, catalog index, and privacy migrations

---
+++ migrations/0013_ask_for_key.sql
@@ -0,0 +1,1 @@
+alter table reviews add column if not exists ask_for_key boolean not null default false;
---
+++ migrations/0014_privacy_choices.sql
@@ -0,0 +1,1 @@
+alter table profiles add column if not exists allow_sale_and_ads boolean not null default true;
